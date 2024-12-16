5d231730d07d0bf353908f292950aa9f9d047d61
Vinh1VT <vinhvotuan@gmail.com>
2024-12-16 16:50:17 +0100

    clean fibonacci series generation (the first version)

--- main/fibo.s
+++ main/fibo.s
@@ -2,16 +2,23 @@
 
 .text
 
-fibo :
-addi sp, sp, -28
+fibo:
+#prologue
+addi sp, sp, -20
 sw ra, 0(sp)
-sw a0, 4(sp)
-sw a1, 8(sp)
-sw a2, 12(sp)
+sw a0, 4(sp) 
+sw a1, 8(sp) 
+sw a2, 12(sp) 
 sw a3, 16(sp)
 sw a4, 20(sp)
-sw a5, 24(sp)
-sw a6, 28(sp)
+
+#####REGISTRES#####
+#a0 adresse du tableau
+#a1 taille du tableau
+#a2 entier rentré dans le tableau
+#a3 valeur a additionner 1
+#a4 valeur a additionner 2
+###################
 
 #initialisation du tableau
 li a2, 0
@@ -19,31 +26,30 @@ sw a2, 0(a0)
 li a2, 1
 sw a2, 4(a0)
 
-addi a3, a1, -2
-li a4, 0
+addi a1, a1, -2
 
+#remplissage apres le terme 2
 boucle:
-beq a4,a3, end
+beqz a1, end
 
-lw a5, 0(a0)
-lw a6, 4(a0)
-add a2, a5, a6
+lw a3, 0(a0)
+lw a4, 4(a0)
+add a2, a3, a4
 sw a2, 8(a0)
 
 
 addi a0,a0,4
-addi a4, a4, 1
+addi a1, a1, -1
 j boucle
 
 
 end:
-lw a6,28(sp)
-lw a5,24(sp)
+#epilogue
 lw a4, 20(sp)
 lw a3, 16(sp)
 lw a2, 12(sp)
 lw a1, 8(sp)
 lw a0, 4(sp)
 lw ra, 0(sp)
-addi sp, sp, 28
+addi sp, sp, 20
 jalr ra
